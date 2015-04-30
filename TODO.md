cbbb477b154f718ee3dd810756b5ddb7bbb301eb
velliz <nurcholid@windowslive.com>
2015-04-30 16:39:14 +0700

    adding first configuration

---
+++ TODO.md
@@ -0,0 +1,7 @@
+Anywhere Version 0.1
+
+Web Service to Simplyfy database connection into only URL request
+
+Feature to impements
+- connect to database and auto generate and save schema based on data inputs.
+- CRUD any data based on url.
---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *  Anywhere version 0.1
+ *  Author : Didit Velliz
+ **/
+
+/*
+ * Database configuration for anywhere.
+ */
+$dbengine = 'MySQL'; // only support MySQL for now
+$username = '';
+$password = '';
+$database = '';
+
+/*
+ * using key for anywhere security.
+ * default is : 12345 you can cange it to anything
+ */
+$key = '12345';
+
+//TODO: another configuration variable
+
+
+
+?>
