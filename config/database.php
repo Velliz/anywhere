ecd509fb123a49aebedaba3176260cc91cf72dce
velliz <nurcholid@windowslive.com>
2015-05-22 13:49:00 +0700

    configure db

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+    $conn = new PDO('mysql:host=localhost;dbname=myDatabase', $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+
+?>
