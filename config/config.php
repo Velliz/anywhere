8d9706e5d4affe3f30f301172a3454d2a2510ee4
Didit Velliz <diditvelliz@outlook.com>
2016-01-07 10:04:15 -0800

    alter connection for mysql

--- config/config.php
+++ config/config.php
@@ -1,25 +1,20 @@
 <?php
+namespace config\core;
+
 /**
  *  Anywhere version 0.1
  *  Author : Didit Velliz
  **/
 
-/*
- * Database configuration for anywhere.
- */
-$dbengine = 'MySQL'; // only support MySQL for now
-$username = '';
-$password = '';
-$database = '';
-
-/*
- * using key for anywhere security.
- * default is : 12345 you can cange it to anything
- */
-$key = '12345';
-
 //TODO: another configuration variable
-
-
+$anywhereBaseConfig = array(
+    'DATABASE_TYPE' => 'mysql',
+    'HOSTNAME' => 'localhost',
+    'USERNAME' => 'root',
+    'PASSWORD' => '',
+    'DISPLAY_ERROR' => true,
+    'BREAK_PROCESS' => false,
+    'DATABASE_NAME' => 'bakery'
+);
 
 ?>
