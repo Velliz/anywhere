59b17b056dc27f7bc9d42de877d30b4057b54244
Velliz <diditvelliz@gmail.com>
2016-07-19 11:06:53 +0700

    invalid pwd

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return array(
     'dbType' => 'pgsql',
     'host' => 'ec2-54-221-234-118.compute-1.amazonaws.com',
     'user' => 'nomuqweikqjney',
-    'pass' => ' 7N9Bs05jQOy9cXucoMFJ1tb8NM',
+    'pass' => '7N9Bs05jQOy9cXucoMFJ1tb8NM',
     'dbName' => 'db1h21rrq8vg2v',
     'port' => 5432
 );
