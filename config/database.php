94e2ac9b880493372f64aaaa9ca5da736f8cbbd1
Velliz <diditvelliz@gmail.com>
2016-10-11 16:12:10 +0700

    initialize

--- config/database.php
+++ config/database.php
@@ -3,8 +3,8 @@
 return array(
     'dbType' => 'mysql',
     'host' => 'localhost',
-    'user' => 'root',
-    'pass' => '',
+    'user' => 'adminUr5Apjy',
+    'pass' => 'mTWNlpNUc8Yi',
     'dbName' => 'anywhere',
     'port' => 3306
 );
