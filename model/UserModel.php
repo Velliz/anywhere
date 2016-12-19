e0f64a3a5e367ee10f49114f0a0c91bdbba1d527
Velliz <diditvelliz@gmail.com>
2016-12-19 14:31:09 +0700

    fix username '=''or' security holes

--- model/UserModel.php
+++ model/UserModel.php
@@ -9,13 +9,13 @@ class UserModel extends DBI
 
     public static function GetUser($username, $password)
     {
-        return DBI::Prepare("SELECT u.*, s.status FROM users u LEFT JOIN status s ON (s.ID = u.statusID) WHERE username = @1 AND password = @2 LIMIT 1;")
+        return DBI::Prepare("SELECT u.*, s.status FROM users u LEFT JOIN status s ON (s.ID = u.statusID) WHERE (username = @1) AND (password = @2) LIMIT 1;")
             ->GetData($username, $password);
     }
 
     public static function GetUserById($id)
     {
-        return DBI::Prepare("SELECT u.*, s.status FROM users u LEFT JOIN status s ON (s.ID = u.statusID) WHERE u.ID = @1 LIMIT 1;")
+        return DBI::Prepare("SELECT u.*, s.status FROM users u LEFT JOIN status s ON (s.ID = u.statusID) WHERE (u.ID = @1) LIMIT 1;")
             ->GetData($id);
     }
 
