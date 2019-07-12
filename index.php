c78698e1c2cc8ff568b59f307429d9f3f0f530c8
Didit Velliz <diditvelliz@gmail.com>
2019-07-12 15:10:01 +0700

    adding cors rules

--- index.php
+++ index.php
@@ -21,6 +21,11 @@ if (isset($_SERVER['HTTPS'])) {
     }
 }
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Authorization, X-Permissions");
+
 $factory = array(
     'base' => ($protocol . "://" . $_SERVER['HTTP_HOST'] . "/"),
     'root' => __DIR__,
