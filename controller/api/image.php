8d50be7a0f896032d684ad52b0c7abd1838becf2
Didit Velliz <diditvelliz@gmail.com>
2020-02-12 10:15:35 +0700

    image binary fix with file get contents because database binary auto fetch function dropped

--- controller/api/image.php
+++ controller/api/image.php
@@ -46,21 +46,21 @@ class image extends Service
         if ($type == 'placeholder') {
 
             $name = $_FILES['placeholderfile']['name'];
-            $tmp_name = $_FILES['placeholderfile']['tmp_name'];
+            $tmp_name = file_get_contents($_FILES['placeholderfile']['tmp_name']);
 
             $input = array(
                 'placeholdername' => $name,
                 'placeholderfile' => $tmp_name
             );
 
-            $id = ImageModel::UpdateImagePage(array('IMAGEID' => $id), $input, true);
+            $id = ImageModel::UpdateImagePage(array('IMAGEID' => $id), $input);
             $data['Image'] = ImageModel::GetImageAttribute($id);
 
             return $data;
         } elseif ($type == 'sample') {
 
             $name = $_FILES['samplefile']['name'];
-            $tmp_name = $_FILES['samplefile']['tmp_name'];
+            $tmp_name = file_get_contents($_FILES['samplefile']['tmp_name']);
 
             $input = array(
                 'IMAGEID' => $id,
@@ -68,7 +68,7 @@ class image extends Service
                 'requestsamplefile' => $tmp_name
             );
 
-            $id = ImageModel::UpdateImagePage(array('IMAGEID' => $id), $input, true);
+            $id = ImageModel::UpdateImagePage(array('IMAGEID' => $id), $input);
             $data['Image'] = ImageModel::GetImageAttribute($id);
 
             return $data;
