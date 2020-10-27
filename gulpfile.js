d2d9c5fe8c0743bcc9990ad4f4df3b14dbe8e73c
Didit Velliz <diditvelliz@gmail.com>
2020-10-27 23:53:40 +0700

    proxy revision

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp'),
     connect = require('gulp-connect-php'),
     browserSync = require('browser-sync');
 
-gulp.task('connect-sync', function () {
-    connect.server({}, function () {
+gulp.task('connect-sync', function() {
+    connect.server({}, function (){
         browserSync({
-            proxy: '127.0.0.1:8000'
+            proxy: 'anywhere.test'
         });
     });
 
