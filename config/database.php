5062aef8b5243282942c0ecb24c25928c90a410a
Didit Velliz <diditvelliz@gmail.com>
2026-01-19 08:58:07 +0700

    implemant ajax loading interface part2

--- config/database.php
+++ config/database.php
@@ -13,20 +13,4 @@ $db['primary'] = [
     ]
 ];
 
-
-$db['primary'] = [
-    'dbType' => 'mysql',
-    'host' => 'localhost',
-    'user' => 'root',
-    'pass' => '',
-    'dbName' => 'anywhere',
-    'port' => '3306',
-    'driver' => '',
-    'ignoreTableWithPrefix' => '_',
-    'hideColumns' => [
-        'created', 'modified', 'cuid', 'muid', 'dflag', 'password'
-    ]
-];
-
-
 return $db;
